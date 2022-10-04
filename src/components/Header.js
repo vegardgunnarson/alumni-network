cc4e1397204012e648487662a51f05949973fa49
vegardgunnarson <110391678+vegardgunnarson@users.noreply.github.com>
2022-10-04 15:51:39 +0200

    add searchbar to timeline

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
+import "../styles/Header.scss";
 
 export default function Header() {
   return (
     <header>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item active">
               <a class="nav-link" href="/timeline">
@@ -17,8 +17,10 @@ export default function Header() {
               </a>
             </li>
           </ul>
-        
       </nav>
+      <div class="p-1 text-center bg-light">
+    <h1 class="mb-3">Alumni</h1>
+  </div>
     </header>
   );
 }
--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,11 +1,34 @@
 import React from "react";
+import "../styles/Timeline.scss";
 
+export default function Timeline() {
+    /*const searchButton = document.getElementById('search-button');
+    const searchInput = document.getElementById('search-input');
+    searchButton.addEventListener('click', () => {
+      const inputValue = searchInput.value;
+      alert(inputValue);
+    });*/
 
-
-export default function Timeline () {
-    return (
-        <div class="mt-auto py-3 text-center text-lg-start bg-light">
-            <span class="text-dark">//TODO Timeline page</span>
+  return (
+    <div className="timeline">
+    <div className="search">
+      <div class="input-group" className="searchbar">
+        <div class="form-outline">
+          <input
+            id="search-input"
+            type="search"
+            class="form-control"
+          />
         </div>
-    )
+        <button id="search-button" type="button" class="btn btn-secondary">
+          <i class="fas fa-search">Search</i>
+        </button>
+      </div>
+    </div>
+    <div class="container">
+        //TODO timeline objects
+    </div>
+    </div>
+    
+  );
 }
