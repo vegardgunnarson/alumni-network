0b80e6bde70e26e5129268aa781cfa7092f2b9a5
vegardgunnarson <110391678+vegardgunnarson@users.noreply.github.com>
2022-10-10 10:00:29 +0200

    back-end-Ready

---
+++ src/components/UserHandler.js
@@ -0,0 +1,96 @@
+
+const apiKey =  process.env.REACT_APP_API_KEY
+const apiUrl = process.env.REACT_APP_API_URL
+
+/**
+ * Function responsible for updating a users information using API PATCH request.
+ * @param {Object} user - A user object
+ * @param {String} newStatus - An array containing the new translations
+ * @returns Patched user object or error message
+ */
+ export const updateStatus = async (user, newStatus) => {
+    try {
+        const response = await fetch(`${apiUrl}/status/${user.id}`, {
+            method: 'PATCH',
+            headers: {
+              'X-API-Key': apiKey,
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                status: newStatus
+            })
+        })
+
+        if (!response.ok) {
+            throw new Error ('Could not update status')
+        }
+
+        const result = await response.json();
+        return [null, result]
+
+    } catch (error) {
+        return [error.message, null];
+    }
+}
+
+/**
+ * Function responsible for updating a users information using API PATCH request.
+ * @param {Object} user - A user object
+ * @param {String} newBio - An array containing the new translations
+ * @returns Patched user object or error message
+ */
+ export const updateBio = async (user, newBio) => {
+    try {
+        const response = await fetch(`${apiUrl}/bio/${user.id}`, {
+            method: 'PATCH', 
+            headers: {
+              'X-API-Key': apiKey,
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                bio: newBio
+            })
+        })
+
+        if (!response.ok) {
+            throw new Error ('Could not update bio')
+        }
+
+        const result = await response.json();
+        return [null, result]
+
+    } catch (error) {
+        return [error.message, null];
+    }
+}
+
+/**
+ * Function responsible for updating a users information using API PATCH request.
+ * @param {Object} user - A user object
+ * @param {String} newFunfact - An array containing the new translations
+ * @returns Patched user object or error message
+ */
+ export const updateFunfact = async (user, newFunfact) => {
+    try {
+        const response = await fetch(`${apiUrl}/funfact/${user.id}`, {
+            method: 'PATCH', // 
+            headers: {
+              'X-API-Key': apiKey,
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                funfact: newFunfact
+            })
+        })
+
+        if (!response.ok) {
+            throw new Error ('Could not update funfact')
+        }
+
+        const result = await response.json();
+        return [null, result]
+
+    } catch (error) {
+        return [error.message, null];
+    }
+}
