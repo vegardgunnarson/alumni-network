23c40a3ebc08ae70322d22175c57cac08139d2a8
Legazzzy <marius.eriksen13@yahoo.no>
2022-10-17 15:20:59 +0200

    login working, adds multiple objects in db

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,9 +1,6 @@
-import React from "react";
 import keycloak from "../keycloak/keycloak";
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { createUser } from "../api/user";
-import axios, { createHeaders } from "../api/index";
 
 const apiUrl = process.env.REACT_APP_API_URL+"student";
 
@@ -14,42 +11,20 @@ const GetLoggedIn = () => {
 
 
     useEffect( () => {
-    
-    goingToLogIn()
-    
+        logginmukk();
+        nav('/timeline');
+
     }, []);
 
+    const logginmukk = async () => {
+    while(!keycloak.authenticated)
+       await goingToLogIn()
 
+    }
     const goingToLogIn = async () => {
-
-        
-        if(!keycloak.authenticated){
+        await keycloak.login();    
     
-        keycloak.login();
-
-        }
-        
-        try {
-              const response = await fetch(apiUrl, {
-                  method: 'POST',
-                  headers: createHeaders(),
-                  body: JSON.stringify({
-                    token: keycloak.tokenParsed.preferred_username,
-                    name: keycloak.tokenParsed.name,
-                  })
-              });
-        
-              const data = await response.json();
-              //return user object
-              return data;
-        
-          } catch (error) {
-              return error.message;
-          }
-        } 
-        nav('/timeline')
-    
-    
+}
 }
 
 
