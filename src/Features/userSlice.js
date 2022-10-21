3687e1564151cacca3da9b5cdcc8df71c0648353
Legazzzy <marius.eriksen13@yahoo.no>
2022-10-21 13:35:58 +0200

    editing profile

--- src/Features/userSlice.js
+++ src/Features/userSlice.js
@@ -6,7 +6,9 @@ const initialState = {
 }
 
 export const userSlice = createSlice({
+    id: 'id',
     name: 'user',
+    picture: 'https://i.pinimg.com/736x/d7/3b/c1/d73bc1381fb1380112a98266b6f2d27f.jpg',
     initialState,
     reducers: {
       updateUser: (state, action) => {
@@ -15,8 +17,14 @@ export const userSlice = createSlice({
       resetUser: (state) => {
         state.value = {};
       },
-      updateDescription: (state, action) => {
+      updateBio: (state, action) => {
         state.value.description = action.payload;
+      },
+      updateStatus: (state, action) => {
+        state.value.status = action.payload;
+      },
+      updateFunfact: (state, action) => {
+        state.value.funfact = action.payload;
       },
   
     },
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,56 +1,67 @@
 import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import keycloak from "../keycloak/keycloak";
 
 
 import "../styles/Profile.scss";
 import { currentuser} from "./UserHandler";
+import { selectUser, updateUser } from "../Features/userSlice"
+import axios from "axios";
+const apiUrl = process.env.REACT_APP_API_URL;
 
 export default function Profile() {
 
+    const thisUser = useSelector(selectUser)
+    const dispatch = useDispatch()
 
-    const [user, setUser] = useState([]);
-
-    const fetchData = () => {
-        return fetch(`https://alumni-case-database.herokuapp.com/api/v1/student/${currentuser.id}`)
-            .then((response) => response.json())
-            .then((data => setUser(data)))
-    }
-
-    useEffect(() => {
-        fetchData();
-    },[])
     
     const [updateuser, setUpdateuser] = useState({
-        status: user.status,
-        bio: user.bio,
-        fun_fact: user.fun_fact
+        id:thisUser.id,
+        status: thisUser.status,
+        bio: thisUser.bio,
+        fun_fact: thisUser.fun_fact
     });
     const handleChange = (event) => {
         setUpdateuser({...updateuser, [event.target.name]:event.target.value})
+        console.log(updateuser)
     }
     const handlesubmit = (event) => {
         event.preventDefault();
         console.log(updateuser);
-        setUpdateuser({status:user.status, bio:user.bio, fun_fact:user.fun_fact})
+        setUpdateuser({status:updateuser.status, bio:updateuser.bio, fun_fact:updateuser.fun_fact})
+        updateUserInDb()
+        dispatch(updateUser(getUser()))
+
+
+
+    const updateUserInDb = async () => {
+        const response = await axios.put(`${apiUrl}student${thisUser.id}`, updateuser)
+        if(response.status !== 201){
+          console.log(response)
+        }
+        return response.data
     }
+    const getUser = async () => {
+        const response = await axios.get(`${apiUrl}student${updateuser.id}`)
+        console.log(response.data)
+    }
+}
     
     
 
   return (
     <div class="mt-auto py-3 text-center text-lg-start" className="profile">
-        <form class="container-lg" className="form">
-            <div className="nameAndPicture">
-            <img src={user.picture} alt="could not be found" className="image" />
-            <div class="form-control" id="name" className="name">{user.name}</div>
-            </div>
-            <textarea type="input" name="status" placeholder="Status" onChange={handleChange} value={updateuser.status} id="status" className="status"></textarea>
+        <div className="nameAndPicture">
+            <img src={thisUser.picture} alt="could not be found" className="image" />
+        <div class="form-control" id="name" className="name">{thisUser.id}</div>
+        </div>
+            <input type="input" name="status" placeholder="Status" onChange={handleChange} value={updateuser.status} id="status" className="status"></input>
             <input  name="bio" placeholder="Bio" id="bio" onChange={handleChange} className="bio" value={updateuser.bio}></input><br/>
             <input  name="funfact" placeholder="Funfact" onChange={handleChange} id="funfact" value={updateuser.fun_fact} className="funfact"></input><br/>
-          <div class="nav-item" className="buttons">
-          <button type="submit" class="btn btn-secondary m-4" onSubmit={handlesubmit}>Save</button>
-          <button class="btn btn-danger m-4" onClick={() => keycloak.logout()}>Logout</button>
-          </div>
-        </form>
+        <div class="nav-item" className="buttons">
+            <button type="submit" class="btn btn-secondary m-4" onSubmit={handlesubmit}>Save</button>
+            <button class="btn btn-danger m-4" onClick={() => keycloak.logout()}>Logout</button>
+        </div>
     </div>
   );
 }
