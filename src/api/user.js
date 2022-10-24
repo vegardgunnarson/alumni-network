c2cb878573b58e59231b6cd1d0c2498a73500453
Legazzzy <marius.eriksen13@yahoo.no>
2022-10-24 14:22:49 +0200

    update user and user picture

--- src/api/user.js
+++ src/api/user.js
@@ -1,58 +1,65 @@
 import axios, { createHeaders } from ".";
 import keycloak from "../keycloak/keycloak";
 
-const apiUrl = process.env.REACT_APP_API_URL+"student";
+const apiUrl = process.env.REACT_APP_API_URL + "student";
 
 export const createUser = async (username) => {
   //post new user with username
   //alert("hei")
   try {
     //alert()
-      const response = await fetch(apiUrl, {
-          method: 'POST',
-          headers: createHeaders(),
-          body: JSON.stringify({
-            id: username,
-            name: keycloak.tokenParsed.name,
-            picture: "https://i.pinimg.com/736x/d7/3b/c1/d73bc1381fb1380112a98266b6f2d27f.jpg",
-            status: "status",
-            bio: "bio",
-            fun_fact: "ffact"
-          })
-      });
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: createHeaders(),
+      body: JSON.stringify({
+        id: username,
+        name: keycloak.tokenParsed.name,
+        picture:
+          "https://i.pinimg.com/736x/d7/3b/c1/d73bc1381fb1380112a98266b6f2d27f.jpg",
+        status: "status",
+        bio: "bio",
+        fun_fact: "ffact",
+      }),
+    });
 
-      if(!response.ok) {
-          throw new Error('Could not create new user with username '+keycloak.tokenParsed.username);
-      }
-      const data = await response.json();
-      //return user object
-      return [null, data];
-
+    if (!response.ok) {
+      throw new Error(
+        "Could not create new user with username " +
+          keycloak.tokenParsed.username
+      );
+    }
+    const data = await response.json();
+    //return user object
+    return [null, data];
   } catch (error) {
-      return [error, null];
+    return [error, null];
   }
-}
+};
 
 export const checkDbForUser = async () => {
   try {
-      const username = keycloak.tokenParsed.preferred_username;
-      const response = await fetch(`${apiUrl}/${username}`)
-      console.log(response)
-      if (!response.ok){
-        await createUser(username);
-        const response = await fetch(`${apiUrl}/${username}`)
-        const data = await response.json();
-        console.log(data)
-        return [null, data]
-
-
-      } else {
-        const data = await response.json();
-        console.log(data)
-        return [null, data]
-      }
+    const username = keycloak.tokenParsed.preferred_username;
+    const response = await fetch(`${apiUrl}/${username}`);
+    console.log(response);
+    if (!response.ok) {
+      await createUser(username);
+      const response = await fetch(`${apiUrl}/${username}`);
+      const data = await response.json();
+      console.log(data);
+      return [null, data];
+    } else {
+      const data = await response.json();
+      console.log(data);
+      return [null, data];
+    }
+  } catch (error) {
+    return [error, null];
   }
-  catch (error) {
-  return [error, null]
-}
-}
+};
+
+export const getUserFromDb = async () => {
+  const username = keycloak.tokenParsed.preferred_username;
+  const dbUser = await fetch(`${apiUrl}/${username}`);
+  const data = await dbUser.json();
+  return data;
+};
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,77 +2,137 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import keycloak from "../keycloak/keycloak";
 
-import { updateThisUser } from "../util/createUser"
-
 import "../styles/Profile.scss";
-import { currentuser} from "./UserHandler";
-import { selectUser, updateUser } from "../Features/userSlice"
+import { selectUser, updateUser } from "../Features/userSlice";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { getUserFromDb } from "../api/user";
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export default function Profile() {
-    const nav = useNavigate()
-    const thisUser = useSelector(selectUser)
-    const dispatch = useDispatch()
+    const thisUser = useSelector(selectUser);
+    const dispatch = useDispatch();
 
     const [updateuser, setUpdateuser] = useState({
         status: thisUser.status,
         bio: thisUser.bio,
-        fun_fact: thisUser.fun_fact
+        fun_fact: thisUser.fun_fact,
     });
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
+
+    useEffect(() => {}, [thisUser]);
+
+    const fetchUser = async () => {
+        dispatch(updateUser(await getUserFromDb()));
+    };
     const handleChange = (event) => {
-        setUpdateuser({...updateuser, [event.target.name]:event.target.value})
-    }
+        setUpdateuser({ ...updateuser, [event.target.name]: event.target.value });
+    };
 
     const handlesubmit = async () => {
-        dispatch(updateUser( await updateUserInDb()))
-    }
+        dispatch(updateUser(await updateUserInDb()));
+        console.log(thisUser);
+    };
 
+    const changePicture = async () => {
+        const pictureUrl = prompt("write picture url");
+
+        const newUser = {
+        id: thisUser.id,
+        name: thisUser.name,
+        picture: pictureUrl,
+        status: updateuser.status,
+        bio: updateuser.bio,
+        fun_fact: updateuser.funfact,
+        createdAlumniEvents: thisUser.createdAlumniEvents,
+        alumniGroups: thisUser.alumniGroups,
+        alumniEvents: thisUser.alumniEvents,
+        topics: thisUser.topics,
+        posts: thisUser.posts,
+        ownedAlumniGroups: thisUser.ownedAlumniGroups,
+        studentMembershipInvites: thisUser.studentMembershipInvites,
+        studentRSVPs: thisUser.studentRSVP,
+        };
+
+        await axios.put(`${apiUrl}student/${thisUser.id}`, newUser);
+        return newUser;
+    };
 
     const updateUserInDb = async () => {
-        console.log("HELLO");
-        const newUser = {  	
-            id: thisUser.id,
-            name: thisUser.name,
-            picture: thisUser.picture,
-            status: updateuser.status,
-            bio: updateuser.bio,
-            fun_fact: updateuser.funfact,
-            createdAlumniEvents: thisUser.createdAlumniEvents,
-            alumniGroups: thisUser.alumniGroups,
-            alumniEvents: thisUser.alumniEvents,
-            topics: thisUser.topics,
-            posts: thisUser.posts,
-            ownedAlumniGroups: thisUser.ownedAlumniGroups,
-            studentMembershipInvites: thisUser.studentMembershipInvites,
-            studentRSVPs:thisUser.studentRSVP 
-        }
+        const newUser = {
+        id: thisUser.id,
+        name: thisUser.name,
+        picture: thisUser.picture,
+        status: updateuser.status,
+        bio: updateuser.bio,
+        fun_fact: updateuser.funfact,
+        createdAlumniEvents: thisUser.createdAlumniEvents,
+        alumniGroups: thisUser.alumniGroups,
+        alumniEvents: thisUser.alumniEvents,
+        topics: thisUser.topics,
+        posts: thisUser.posts,
+        ownedAlumniGroups: thisUser.ownedAlumniGroups,
+        studentMembershipInvites: thisUser.studentMembershipInvites,
+        studentRSVPs: thisUser.studentRSVP,
+        };
 
-        const link = `${apiUrl}student/${thisUser.id}`
-        const response = await axios.put(`${apiUrl}student/${thisUser.id}`, newUser)
-        return newUser
-        console.log(response);
+        await axios.put(`${apiUrl}student/${thisUser.id}`, newUser);
+        return newUser;
+    };
 
-    }
-    
-
-    
-    
-
-  return (
-    <div class="mt-auto py-3 text-center text-lg-start" className="profile">
+    return (
+        <div class="mt-auto py-3 text-center text-lg-start" className="profile">
         <div className="nameAndPicture">
-            <img src={thisUser.picture} alt="could not be found" className="image" />
-        <div class="form-control" id="name" className="name">{thisUser.id}</div>
+            <img
+            src={thisUser.picture}
+            alt="could not be found"
+            onClick={() => changePicture()}
+            className="image"
+            />
+            <div class="form-control" id="name" className="name">
+            {thisUser.id}
+            </div>
         </div>
-            <input  name="status" placeholder="Status" onChange={handleChange} value={updateuser.status} id="status" className="status"></input>
-            <input  name="bio" placeholder="Bio" id="bio" onChange={handleChange} className="bio" value={updateuser.bio}></input><br/>
-            <input  name="funfact" placeholder="Funfact" onChange={handleChange} id="funfact" value={updateuser.fun_fact} className="funfact"></input><br/>
+        <input
+            name="status"
+            placeholder="Status"
+            onChange={handleChange}
+            value={updateuser.status}
+            id="status"
+            className="status"
+        ></input>
+        <input
+            name="bio"
+            placeholder="Bio"
+            id="bio"
+            onChange={handleChange}
+            className="bio"
+            value={updateuser.bio}
+        ></input>
+        <br />
+        <input
+            name="funfact"
+            placeholder="Funfact"
+            onChange={handleChange}
+            id="funfact"
+            value={updateuser.fun_fact}
+            className="funfact"
+        ></input>
+        <br />
         <div class="nav-item" className="buttons">
-            <button type="submit" class="btn btn-secondary m-4" onClick={handlesubmit}>Save</button>
-            <button class="btn btn-danger m-4" onClick={() => keycloak.logout()}>Logout</button>
+            <button
+            type="submit"
+            class="btn btn-secondary m-4"
+            onClick={handlesubmit}
+            >
+            Save
+            </button>
+            <button class="btn btn-danger m-4" onClick={() => keycloak.logout()}>
+            Logout
+            </button>
         </div>
-    </div>
-  );
+        </div>
+    );
 }
